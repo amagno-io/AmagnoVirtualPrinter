ed1788f9a2b6a6fa987b53a4682b9cd65ed37683
Marco Batzinger <mba@amagno.de>
2021-04-15 13:31:31 +0200

    adjusted create_msi.ps1

--- create_msi.ps1
+++ create_msi.ps1
@@ -1,2 +1,3 @@
 $args = '/MSBUILD:..\Installer\AmagnoVirtualPrinter.WixSharpInstaller', '/p:..\AmagnoVirtualPrinter'
+Remove-Item -Path '.\Files\*' -Filter '*.pdb' -Force
 Start-Process -FilePath '.\Files\AmagnoPrinterInstaller.exe' -ArgumentList $args
