c7d2a2c4fe46f73b33d0c97d1dcb3af0edd2aa82
Gerrit <ged@amagno.de>
2023-05-30 14:37:14 +0200

    Better filesystem handling of create_msi.ps1

--- create_msi.ps1
+++ create_msi.ps1
@@ -1,3 +1,5 @@
-$args = '/MSBUILD:..\Installer\AmagnoVirtualPrinter.WixSharpInstaller', '/p:..\AmagnoVirtualPrinter'
-Remove-Item -Path '.\Files\*' -Filter '*.pdb' -Force
-Start-Process -FilePath '.\Files\AmagnoPrinterInstaller.exe' -ArgumentList $args
+Set-Location $PSScriptRoot
+
+$args = "/MSBUILD:$PSScriptRoot\Installer\AmagnoVirtualPrinter.WixSharpInstaller", "/p:$PSScriptRoot"
+Remove-Item -Path "$PSScriptRoot\Files\*" -Filter '*.pdb' -Force
+Start-Process -FilePath "$PSScriptRoot\Files\AmagnoPrinterInstaller.exe" -ArgumentList $args
