02321a6dfe1382eb46f1dc3ce1928b3d85cdf577
Gerrit <ged@amagno.de>
2023-05-31 14:08:04 +0200

    Wait for setup process

--- create_msi.ps1
+++ create_msi.ps1
@@ -2,4 +2,4 @@ Set-Location $PSScriptRoot
 
 $args = "/MSBUILD:$PSScriptRoot\Installer\AmagnoVirtualPrinter.WixSharpInstaller", "/p:$PSScriptRoot"
 Remove-Item -Path "$PSScriptRoot\Files\*" -Filter '*.pdb' -Force
-Start-Process -FilePath "$PSScriptRoot\Files\AmagnoPrinterInstaller.exe" -ArgumentList $args
+Start-Process -FilePath "$PSScriptRoot\Files\AmagnoPrinterInstaller.exe" -ArgumentList $args -wait
