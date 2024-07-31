c0875430e1415cdd059026f68ea3b5fa19c97d3a
andre-amagno <abr@amagno.de>
2024-07-31 14:47:02 +0200

    Harden killing of processes by catching relevant exceptions. (#1)
    * Harden killing of processes by catching relevant exceptions.
    * Add logging in ProgressInfoProcessManager.
    * Add annotations.

--- UI/AmagnoVirtualPrinter.ProgressInfo.Lib/ProgressInfoProcessManager.cs
+++ UI/AmagnoVirtualPrinter.ProgressInfo.Lib/ProgressInfoProcessManager.cs
@@ -1,13 +1,14 @@
-﻿using JetBrains.Annotations;
-
-using System;
+﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using AmagnoVirtualPrinter.Agent.Core.Interfaces;
+using AmagnoVirtualPrinter.Logging;
 using AmagnoVirtualPrinter.ProgressInfo.Lib.Interfaces;
 using AmagnoVirtualPrinter.Utils;
+using JetBrains.Annotations;
 
 namespace AmagnoVirtualPrinter.ProgressInfo.Lib
 {
@@ -15,6 +16,14 @@ namespace AmagnoVirtualPrinter.ProgressInfo.Lib
     public class ProgressInfoProcessManager : IProgressInfoProcessManager
     {
         private const string ProcessName = "AmagnoPrinterAgentProgress";
+
+        [NotNull]
+        private readonly IVirtualPrinterLogger<ProgressInfoProcessManager> _logger;
+
+        public ProgressInfoProcessManager([NotNull]IVirtualPrinterLogger<ProgressInfoProcessManager> logger)
+        {
+            _logger = logger;
+        }
 
         public bool IsRunning()
         {
@@ -37,10 +46,36 @@ namespace AmagnoVirtualPrinter.ProgressInfo.Lib
         {
             var processes = Process.GetProcesses().Where(pList => pList.ProcessName.Contains(ProcessName));
 
-            foreach(var process in processes)
+            foreach (var process in processes)
+            {
+                KillProcess(process);
+            }
+        }
+
+        private void KillProcess([NotNull]Process process)
+        {
+            try
             {
                 process.Kill();
             }
+            catch (Win32Exception exception)
+            {
+                // The associated process could not be terminated. -or-
+                // The process is terminating. -or-
+                // The associated process is a Win16 executable.
+                LogWarn(exception, "Failed to kill process while printing");
+            }
+            catch (InvalidOperationException exception)
+            {
+                // The process has already exited. -or-
+                // There is no process associated.
+                LogWarn(exception, "Failed to kill process while printing");
+            }
+        }
+        
+        private void LogWarn(Exception exception, string message)
+        {
+            _logger.Warn("{message}:\r\nException: {exception}", message, exception);
         }
     }
 }
