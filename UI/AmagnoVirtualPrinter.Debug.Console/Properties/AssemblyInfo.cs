1ab4399b2a8c93ad54ebf64a9b2d6f90885aaf1f
Gerrit Edzards <ged@amagno.de>
2024-08-27 13:50:19 +0200

    Rename PrinterDebug.Console to AmagnoVirtualPrinter.Debug.Console

--- UI/AmagnoVirtualPrinter.Debug.Console/Properties/AssemblyInfo.cs
+++ UI/AmagnoVirtualPrinter.Debug.Console/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrinterDebug.Console")]
+[assembly: AssemblyTitle("AmagnoVirtualPrinter.Debug.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrinterDebug.Console")]
+[assembly: AssemblyProduct("AmagnoVirtualPrinter.Debug.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
