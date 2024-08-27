2c96e5cff19cd4ec3cb366bab87d2b27b833fdf6
Gerrit Edzards <ged@amagno.de>
2024-08-27 16:18:52 +0200

    Downgrade WiX

--- create_msi.ps1
+++ create_msi.ps1
@@ -1,5 +1,9 @@
 Set-Location $PSScriptRoot
 
+# https://github.com/actions/runner-images/issues/9667
+choco uninstall wixtoolset -y
+choco install wixtoolset --version=3.11.2 --force -y
+
 $arguments = "/MSBUILD:$PSScriptRoot\Installer\AmagnoVirtualPrinter.WixSharpInstaller", "/p:$PSScriptRoot"
 Remove-Item -Path "$PSScriptRoot\Files\*" -Filter '*.pdb' -Force
 Start-Process -FilePath "$PSScriptRoot\Files\AmagnoPrinterInstaller.exe" -ArgumentList $arguments -wait
